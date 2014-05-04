9784d573adc8bcb84b8ee2fafaed79459bec3863
Ectelion <rustemb712@gmail.com>
2014-05-04 02:27:00 +0200

    wireframe of the function with comments

---
+++ rankhospital.R
@@ -0,0 +1,14 @@
+## The function reads the "Outcome-of-care-measures" dataset and returns 
+## a character vector with the name of the hospital that has the ranking 
+## specified by the num argument
+
+rankhospital <- function(state, outcome, num = "best") {
+	## Read outcome data
+
+	## Check that state and outcome are valid
+
+	## Return hospital name in that state with the given rank
+
+	## 30-day death rate
+
+}
